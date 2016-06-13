f0785bc351d7e3262b78fd480b30c81646d39a1d
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-06-13 17:41:24 -0400

    added jquery dotdotdot for multiline ellipses

--- app/assets/javascripts/components/addon/addon.js.jsx
+++ app/assets/javascripts/components/addon/addon.js.jsx
@@ -1,10 +1,19 @@
 var Addon = React.createClass({
+
+  setTaglineRef: function (ref) {
+    this.tagline = ref;
+  },
+
+  componentDidMount: function () {
+    $(this.tagline).dotdotdot();
+  },
+
   render: function() {
     return (
       <a href={this.props.data.link} className="addon">
         <img src={this.props.data.icon} className="addon-icon"/>
         <div className="addon-name">{this.props.data.name}</div>
-        <div className="addon-tagline">{this.props.data.tagline}</div>
+        <div className="addon-tagline" ref={this.setTaglineRef}>{this.props.data.tagline}</div>
       </a>
     );
   }
