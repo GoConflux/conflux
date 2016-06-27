eff42f3d796712036ac323873ec59036e192efef
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-06-27 16:35:48 -0400

    added home sections 1 and 2

--- app/assets/javascripts/components/pages/home.js.jsx
+++ app/assets/javascripts/components/pages/home.js.jsx
@@ -10,9 +10,37 @@ var Home = React.createClass({
       <div id="home">
         <div className="home-header">
           <div className="title">Conflux Add-ons Platform & Marketplace</div>
-          <div className="subtitle">Manage all of your app's third-party services from one place, regardless of host platform or environment.</div>
+          <div className="subtitle">Manage all of your app's third-party services from one secure location, regardless of host platform or environment.</div>
         </div>
-        <div id="homeBody"></div>
+        <div id="homeBody">
+          <div className="home-section">
+            <div className="section-inner">
+              <div className="sub-section">
+                <img className="image-section left" src="http://confluxapp.s3-website-us-west-1.amazonaws.com/images/platform-agnostic.png" />
+              </div>
+              <div className="sub-section">
+                <div className="text-section">
+                  <div className="feature">Use add-ons across any platform.</div>
+                  <div className="sub-text">This is my subtext.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="home-section">
+            <div className="section-inner">
+              <div className="sub-section">
+                <div className="text-section">
+                  <div className="feature">Easily clone & scale add-ons across environments.</div>
+                  <div className="sub-text">This is my subtext.</div>
+                </div>
+              </div>
+              <div className="sub-section">
+                <img className="image-section right" src="http://confluxapp.s3-website-us-west-1.amazonaws.com/images/clone.png" />
+              </div>
+            </div>
+          </div>
+
+        </div>
         <LandingFooter />
       </div>
     );
