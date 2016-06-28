11bcc2108801b9c9d01dbf4c7f061ab557bcdde4
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-06-28 15:51:25 -0400

    added terminal simulation effect for home section 1

--- app/assets/javascripts/components/pages/home.js.jsx
+++ app/assets/javascripts/components/pages/home.js.jsx
@@ -1,8 +1,90 @@
 var Home = React.createClass({
 
-  getInitialState: function () {
-    return {
-    };
+  terminalOutputs: [
+    {
+      text: "Provisioning redistogo add-on...",
+      delay: 1000
+    },
+    {
+      text: "Writing new configs to conflux.yml...",
+      delay: 500
+    },
+    {
+      text: "Found 2 new jobs for redistogo.",
+      delay: 500
+    },
+    {
+      text: "Installing <span style=\"color: #E9573F\">redis</span> ruby gem...",
+      delay: 1000
+    },
+    {
+      text: "<span style=\"color: #59A45C\">Creating initializer file:</span> config/redis.rb...",
+      delay: 500
+    },
+    {
+      text: "<span style=\"color: #0EBEC1\">Successfully configured redistogo.</span>",
+      delay: 0
+    }
+  ],
+
+  setTerminalContainerRef: function (ref) {
+    this.terminalContainer = ref;
+  },
+
+  setTerminalTextRef: function (ref) {
+    this.terminal = ref;
+  },
+
+  componentDidMount: function () {
+    this.startTerminalAnimation();
+  },
+
+  startTerminalAnimation: function () {
+    var self = this;
+
+    $(this.terminal).typed({
+      strings: ['$ conflux addons:add redistogo'],
+      typeSpeed: 0,
+      onStringTyped: function () {
+        setTimeout(function () {
+          $(self.terminalContainer).addClass('with-output');
+          $(self.terminal).append($('<br>'));
+          $(self.terminalContainer).find('.typed-cursor').addClass('new-line');
+          self.showTerminalOutput(0);
+        }, 300);
+      }
+    });
+  },
+
+  showTerminalOutput: function (i) {
+    var self = this;
+    var info = this.terminalOutputs[i];
+    var $output = $('<span>', { html: info.text });
+    $(this.terminal).append($output);
+    $(this.terminal).append($('<br>'));
+
+    setTimeout(function () {
+      i++;
+
+      if (self.terminalOutputs[i]) {
+        self.showTerminalOutput(i);
+      } else {
+        $(self.terminalContainer).find('.typed-cursor').hide();
+
+        setTimeout(function () {
+          self.resetTerminalAnimation();
+        }, 15000);
+      }
+    }, info.delay);
+  },
+
+  resetTerminalAnimation: function () {
+    $(this.terminalContainer).empty();
+    $(this.terminalContainer).removeClass('with-output');
+    var $newTerminalText = $('<div>', { class: 'terminal-text' });
+    this.terminal = $newTerminalText[0];
+    $(this.terminalContainer).append($newTerminalText);
+    this.startTerminalAnimation();
   },
 
   render: function() {
@@ -16,11 +98,26 @@ var Home = React.createClass({
           <div className="home-section">
             <div className="section-inner">
               <div className="sub-section">
+                <div className="text-section">
+                  <div className="feature">Provision & configure add-ons in one command.</div>
+                  <div className="sub-text">This is my subtext.</div>
+                </div>
+              </div>
+              <div className="sub-section">
+                <div className="terminal-example" ref={this.setTerminalContainerRef}>
+                  <div className="terminal-text" ref={this.setTerminalTextRef}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="home-section">
+            <div className="section-inner">
+              <div className="sub-section">
                 <img className="image-section left" src="http://confluxapp.s3-website-us-west-1.amazonaws.com/images/platform-agnostic.png" />
               </div>
               <div className="sub-section">
                 <div className="text-section">
-                  <div className="feature">Access your add-on bundles from any host platform.</div>
+                  <div className="feature">Access add-on bundles from any host platform.</div>
                   <div className="sub-text">This is my subtext.</div>
                 </div>
               </div>
@@ -49,6 +146,19 @@ var Home = React.createClass({
                   <div className="feature">Onboard new team members with one click.</div>
                   <div className="sub-text">This is my subtext.</div>
                 </div>
+              </div>
+            </div>
+          </div>
+          <div className="home-section">
+            <div className="section-inner">
+              <div className="sub-section">
+                <div className="text-section">
+                  <div className="feature">Abstract out config management.</div>
+                  <div className="sub-text">This is my subtext.</div>
+                </div>
+              </div>
+              <div className="sub-section">
+                <img className="image-section right" src="" />
               </div>
             </div>
           </div>
