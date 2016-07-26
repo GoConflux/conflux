2ddd0fdf20e767d5ddaa310c53458873ba04d988
Ben Whittle <benWhittle@bens-mbp.attlocal.net>
2016-07-26 16:48:16 -0400

    fixing issue with inviting same team user back to a team after being destroyed

--- app/services/team_user_services/invite_user.rb
+++ app/services/team_user_services/invite_user.rb
@@ -18,6 +18,7 @@ module TeamUserServices
         # First create the user if they don't exist
         user = User.find_or_initialize_by(email: email)
 
+        # If new user, send a new user email
         if !user.persisted?
           send_email = true
           new_user = true
@@ -26,14 +27,16 @@ module TeamUserServices
         end
 
         # Then create the team_user for them if that doesn't exist
-        team_user = TeamUser.find_or_initialize_by(
+        team_user = TeamUser.unscoped.find_or_initialize_by(
           user_id: user.id,
           team_id: @team.id
         )
 
         team_user.role = @role
 
-        if !team_user.persisted?
+        # If team_user is new, or if it just needs to be undestroyed...
+        if !team_user.persisted? || team_user.is_destroyed
+          team_user.is_destroyed = false
           send_email = true
           team_user.save!
         end
