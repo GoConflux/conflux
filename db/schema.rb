49389986ef3ddd9ffc61b8a89f31b01c9c4bec32
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-09-02 19:53:22 -0700

    added facebook, twitter, and github url columns to addons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830203735) do
+ActiveRecord::Schema.define(version: 20160903025228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,9 @@ ActiveRecord::Schema.define(version: 20160830203735) do
     t.json     "features",            default: []
     t.json     "jobs",                default: {}
     t.json     "api",                 default: {}
+    t.string   "facebook_url"
+    t.string   "twitter_url"
+    t.string   "github_url"
   end
 
   add_index "addons", ["is_destroyed"], name: "index_addons_on_is_destroyed", using: :btree
